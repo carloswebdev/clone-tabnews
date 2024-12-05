26ed42d7189115368aa07c9bdf6b61e76bba10a4
Carlos Eduardo Silva Costa <carloseduardo.silvacosta@gmail.com>
2024-12-04 23:32:50 -0300

    alteração na frase de efeito.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-    return <h1>aki está a frase de efeito</h1>
+function Home() {
+    var retorno =
+        <html>
+            <h1>Você foi agraciado(a) com a oportunidade de apreciar o progresso do meu aprendizado no curso.dev.</h1>
+            <h1>Parabéns!</h1>
+        </html>;
+
+    return retorno;
 }
 
 export default Home;
