9bb00a4a3af9316dc4e93cd12f60e04d28d7967d
Carlos <carloseduardo.silvacosta@gmail.com>
2024-12-07 21:14:04 +0000

    Adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 function Home() {
-    var retorno =
-        <html>
-            <h1>Você foi agraciado(a) com a oportunidade de apreciar o progresso do meu aprendizado no curso.dev.</h1>
-            <h1>Parabéns!</h1>
-        </html>;
+  var retorno = (
+    <html>
+      <h1>
+        Você foi agraciado(a) com a oportunidade de apreciar o progresso do meu
+        aprendizado no curso.dev.
+      </h1>
+      <h1>Parabéns!</h1>
+    </html>
+  );
 
-    return retorno;
+  return retorno;
 }
 
 export default Home;
